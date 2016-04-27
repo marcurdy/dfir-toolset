c660d9a312b220c90868457c907f6b9dcbd161ab
marcurdy <mark.mccurdy@mail.com>
2016-04-27 21:47:32 +0000

    Updating code/docs

---
+++ scripts/timeline-live-linux.sh
@@ -0,0 +1,13 @@
+ls -1d /proc/fs/ext*/* | awk -F/ '{ print $NF }' | while read PART; do
+  echo /dev/$PART
+done | while read DEV; do
+  fls -m/ -r $DEV >> timeline.body
+done
+
+log2timeline.py timeline.pb timeline.body
+
+psort.py -o l2tcsv -w timeline.csv timeline.pb
+
+echo timeline.csv is ready
+
+echo Don\'t forget to cleanup timeline.pb and timeline.body
